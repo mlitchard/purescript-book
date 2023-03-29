dc9c7daaf2aece19281a073d1d67a95e3031b214
The Real Slim Shady <mlitchard@nixosdev>
2023-03-29 19:26:17 +0000

    chapter 2

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -16,17 +16,18 @@ main = do
         Assert.equal 23 (answer 10)
       test "below 1000" do
         Assert.equal 233168 (answer 1000)
-    {-  Move this block comment starting point to enable more tests
     suite "diagonal" do
       test "3 4 5" do
         Assert.equal 5.0 (diagonal 3.0 4.0)
       test "5 12 13" do
         Assert.equal 13.0 (diagonal 5.0 12.0)
+
     suite "circleArea" do
       test "radius 1" do
         Assert.equal 3.141592653589793 (circleArea 1.0)
       test "radius 3" do
         Assert.equal 28.274333882308138 (circleArea 3.0)
+
     suite "leftoverCents" do
       test "23" do
         Assert.equal 23 (leftoverCents 23)
@@ -35,4 +36,4 @@ main = do
       test "-789" do
         Assert.equal (-89) (leftoverCents (-789))
 
--}
+
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Int
+import Data.Number (sqrt,pi)
+
+diagonal a b = sqrt (a * a + b * b) 
+
+circleArea r = pi * (r * r) 
+
+leftoverCents c = rem c 100
